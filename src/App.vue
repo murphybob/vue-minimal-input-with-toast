8d91322eb6ce659d2d171282fc87ba79aaa72280
Rob Murphy <rob.murphy@wegift.io>
2020-07-23 12:37:45 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <MyToggle v-model="valueOfMyToggle" @input="toggleWasToggled"/>
+
+        <p>The value of your toggle is: {{ valueOfMyToggle }}</p>
+
+        <p v-if="showToast">UPDATED TO {{ valueOfMyToggle.toString().toUpperCase() }}</p>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import HelloWorld from './components/HelloWorld.vue';
+    import MyToggle from "@/MyToggle.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    @Component({
+        components: {MyToggle, HelloWorld},
+    })
+    export default class App extends Vue {
+        valueOfMyToggle: boolean = Math.random() > 0.5; // (choose a random start value to show it doesn't matter!)
+        showToast: boolean = false
+        toastTimeout: number | null = null
+
+        toggleWasToggled(value: boolean) {
+            console.log("Parent: I heard that the input had been toggled to", value);
+            this.showToast = true
+            if (this.toastTimeout) clearTimeout(this.toastTimeout)
+            this.toastTimeout = setTimeout(() => this.showToast = false, 2000)
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/MyToggle.vue
@@ -0,0 +1,27 @@
+<template>
+    <label>
+        <span>Toggle this:</span>
+        <input type="checkbox" @input="dothing" :checked="value" />
+    </label>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import {Component, Prop} from "vue-property-decorator";
+
+    @Component
+    export default class MyToggle extends Vue {
+        @Prop({required: true}) value!: boolean
+
+        dothing (ev) {
+            console.log("Input: I am emitting an event to say I have been toggled to", ev.target.checked)
+            this.$emit('input', ev.target.checked)
+        }
+    }
+</script>
+
+<style scoped>
+    label {
+        cursor: pointer
+    }
+</style>
